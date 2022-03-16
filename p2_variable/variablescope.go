025bf149414a6ee6b8adf9088a1941445a4c0478
Habis <devkotahancy@gmail.com>
2022-03-16 17:36:45 +0545

    variable scope

---
+++ p2_variable/variablescope.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var StudentMarks int
+
+func main() {
+
+	StudentMarks = 75
+	fmt.Println(StudentMarks)
+	var StudentId int
+	StudentId = 5
+	fmt.Println(StudentId)
+	fmt.Printf("%v", StudentId)
+}
