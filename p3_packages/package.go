2ac822b9045b9db999642be5af87eac71885c73a
Habis <devkotahancy@gmail.com>
2022-03-16 18:26:42 +0545

    commit on error

--- p3_packages/package.go
+++ p3_packages/package.go
@@ -1,13 +1,13 @@
-package main
+ package main
 
-import (
-	"fmt"
-	"..\Go-Lang Learning\p3_packages"
-)
-func main() {
-	var length float64 = 32.6
-	var breadth float64 = 24.5
+// import (
+// 	"fmt"
+// 	"..\Go-Lang Learning\p3_packages"
+// )
+// func main() {
+// 	var length float64 = 32.6
+// 	var breadth float64 = 24.5
 
-	area := geometry.Area(length, breadth)
-	fmt.Println(length)
-}
+// 	area := geometry.Area(length, breadth)
+// 	fmt.Println(length)
+// }
