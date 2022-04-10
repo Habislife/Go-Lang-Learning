611a53b6573c112990928c8e967a79c44ac3456d
Bishal <devkotahancy@gmail.com>
2022-04-10 23:05:24 +0545

    function intro

--- p7_function/intro.go
+++ p7_function/intro.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	var num1 float64 = 50
+	var num2 float64 = 40
+	av := findAverage(num1, num2)
+	fmt.Println(av)
+
+}
+
+func findAverage(num1, num2 float64) (average float64) {
+	average = (num1 + num2) / 2
+	return average
 }
