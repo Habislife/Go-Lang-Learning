e2226ac600c825ecf8c5bb9843bb2af241fe2c5e
Bishal <devkotahancy@gmail.com>
2022-04-19 17:47:30 +0545

    return multiple value

---
+++ p7_function/return_multiple_value.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x1 float64 = 33
+	var x2 float64 = 55
+	s, d := Calculator(x1, x2)
+	fmt.Println("Sum is : ",s)
+	
+	fmt.Println("Difference is : ",d)
+}
+
+func Calculator(num1, num2 float64) (sum, diff float64) {
+	sum = num1 + num2
+	diff = num1 - num2
+	return sum,diff
+}
